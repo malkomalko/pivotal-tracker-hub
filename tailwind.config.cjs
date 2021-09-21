459c845fc3223c023ed3ce42e47420aea23d3944
Robert Malko <rmalko@doximity.com>
2021-09-21 10:26:56 -0400

    tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 const config = {
 	mode: "jit",
 	purge: [
 		"./src/**/*.{html,js,svelte,ts}",
 	],
 	theme: {
+		colors: {
+			transparent: "transparent",
+			white: "#fff",
+			primary: colors.cyan,
+			secondary: colors.amber,
+			gray: colors.trueGray
+		},
 		extend: {},
 	},
 	plugins: [],
