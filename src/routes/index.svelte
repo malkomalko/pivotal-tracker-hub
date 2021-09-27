7758c8b1df22828f762fba1f0caf98c20d90e4ad
Robert Malko <rmalko@doximity.com>
2021-09-27 10:56:48 -0400

    update more of layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -58,10 +58,27 @@
     {#each activityItems as item}
       <li class="py-4">
         <div class="flex space-x-3">
-          <div class="flex-1 space-y-1">
+          <div class="flex-1 space-y-2">
             <div class="flex items-center justify-between">
-              <h3 class="text-sm font-medium">{item.performed_by.name}</h3>
+              <div class="flex items-center space-x-2">
+                <h3 class="text-sm font-medium">{item.performed_by.name}</h3>
+                <h4 class="text-sm font-medium text-secondary-500">{item.kind}</h4>
+              </div>
               <p class="text-sm text-gray-500">{@html date(item)}</p>
+            </div>
+            <div>
+              <ul class="text-sm">
+              {#each item.primary_resources as pr}
+                <li>
+                  {#if pr.url}
+                    <a class="text-primary-600 font-bold" href={pr.url} target="_blank">{pr.name}</a>
+                  {:else}
+                    {pr.name}
+                  {/if}
+                  on {item.project.name}
+                </li>
+              {/each}
+              </ul>
             </div>
             <p class="text-sm text-gray-500">{item.message}</p>
           </div>
